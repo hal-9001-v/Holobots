be2e439e938d17cb89b1ccc11b880737fd7d5387
dexaxi <dexaxi12@gmail.com>
2021-12-06 20:36:45 +0100

    [FIX] merge fix

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -92,8 +92,8 @@ public class GameDirector : MonoBehaviour
         _teams = new List<Team>();
 
         _teams.Add(new MobTeam());
-        _teams.Add(new AITeam());
-        _teams.Add(new PlayerTeam());
+        _teams.Add(new AITeam(cameraTarget));
+        _teams.Add(new PlayerTeam(cameraTarget));
 
         foreach (var team in _teams)
         {
