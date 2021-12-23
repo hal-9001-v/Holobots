7f379951006ab549e4bf4d363504ef0c492a42f8
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2021-12-23 01:33:34 +0100

    xd

--- Assets/Scripts/Selectables/GroundTile.cs
+++ Assets/Scripts/Selectables/GroundTile.cs
@@ -9,9 +9,10 @@ public class GroundTile : MonoBehaviour
     [Header("Settings")]
     public GroundTile[] neighbours;
     public TileType tileType;
+
     [Range(0, 10)] public int weight = 1;
 
-    public Shield shield { get; private set;}
+    public Shield shield { get; private set; }
 
     public Target unit { get; private set; }
     public Vector2Int cellCoord { get; private set; }
@@ -24,9 +25,58 @@ public class GroundTile : MonoBehaviour
     [HideInInspector]
     public GroundTile parent;
 
+    Vector3 _originalPosition;
+    Vector3 _introStartingPosition;
+    Vector3 _unitIntroStartingPosition;
+
+    [SerializeField] Vector2 _cordsOffset;
+    [SerializeField] [Range(-25, -1)] float _introDepth;
+    [SerializeField] [Range(1, 20)] float _introSpeed;
+
     private void Awake()
     {
         highlightable = GetComponent<Highlightable>();
+
+    }
+
+    void Start()
+    {
+        IntroAnimation();
+    }
+    void IntroAnimation()
+    {
+        _originalPosition = transform.position;
+        _introStartingPosition = _originalPosition + new Vector3(0, 0.1f + _cordsOffset.x * Mathf.Abs(cellCoord.x) + _cordsOffset.y * Mathf.Abs(cellCoord.y), 0) * _introDepth;
+
+        if (unit)
+        {
+            _unitIntroStartingPosition = unit.transform.position;
+        }
+
+        StartCoroutine(IntroAnimationCoroutine());
+    }
+
+    IEnumerator IntroAnimationCoroutine()
+    {
+        float elapsedTime = 0;
+
+        float introTime = Vector3.Distance(_introStartingPosition, _originalPosition) / _introSpeed;
+
+        while (elapsedTime < introTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            transform.position = Vector3.Lerp(_introStartingPosition, _originalPosition, elapsedTime / introTime);
+
+            if (unit)
+            {
+                unit.transform.position = Vector3.Lerp(_introStartingPosition, _unitIntroStartingPosition, elapsedTime / introTime);
+            }
+
+            yield return null;
+        }
+
+        transform.position = _originalPosition;
     }
 
     public void SetShield(Shield shield)
